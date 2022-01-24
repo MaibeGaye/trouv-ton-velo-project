73aed999c76deddfdbe12cca46c1d7840f596c02
Oscar Lienard <lienard.oscar@gmail.com>
2022-01-24 15:38:32 +0100

    fix validation joi

--- app/schemas/signupSchema.js
+++ app/schemas/signupSchema.js
@@ -6,7 +6,7 @@ const schema = Joi.object({
     firstname: Joi.string().min(3).max(64).required(),
     email: Joi.string().email().max(64).required(),
     password : Joi.string().min(8).max(64).required(),
-    passwordConfirm : Joi.ref("password").required(),
+    passwordConfirm : Joi.ref("password"),
     address: Joi.string().alphanum().min(3).max(256).required(),
     zip_code: Joi.number().required()
 });

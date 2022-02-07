6dfe835f8be222e74838e076ac29361131acd157
Oscar <lienard.oscar@gmail.com>
2022-02-07 23:10:56 +0100

    cors open

--- back/index.js
+++ back/index.js
@@ -46,13 +46,15 @@ const options = {
 const jsDoc = expressJSDocSwagger(app);
 jsDoc(options);
 
-app.use(cors({
-  origin: [`https://trouv-ton-velo.surge.sh`,`http://localhost:${port}`],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: true,
-  optionsSuccessStatus: 204
-  //optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}));
+// app.use(cors({
+//   origin: [`https://trouv-ton-velo.surge.sh`,`http://localhost:${port}`],
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   preflightContinue: true,
+//   optionsSuccessStatus: 204
+//   //optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }));
+
+app.use(cors());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({extended: true}));

35ea932935067e02e87b547a9ce577f0b7542263
Oscar <lienard.oscar@gmail.com>
2022-02-08 09:46:17 +0100

    still a cors fix

--- back/index.js
+++ back/index.js
@@ -54,7 +54,16 @@ jsDoc(options);
 //   //optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }));
 
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: true,
+  optionsSuccessStatus: 204
+  //optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
+
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({extended: true}));

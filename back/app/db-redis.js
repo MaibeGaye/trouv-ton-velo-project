68a9b2f59dac28fc07339b49c1136e0b37810d06
MaibeGaye <maibe.gaye@gmail.com>
2022-04-08 15:47:33 +0200

    heroku deploy

--- back/app/db-redis.js
+++ back/app/db-redis.js
@@ -3,7 +3,7 @@ const {createClient} = require('redis');
 const config = {};
 
 if (process.env.NODE_ENV === 'production') {
-    config.url = process.env.REDIS_URL
+    config.url = process.env.REDISCLOUD_URL
 }
 
 const db = createClient(config);
